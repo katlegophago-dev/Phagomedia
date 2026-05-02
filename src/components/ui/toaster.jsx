4ab9cdfe7f6b6b783fa1b602215024f11be55729
katlegophago-dev <katlego.phago7@gmail.com>
2026-05-03 01:23:48 +0200

    Update toaster.jsx

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -3,6 +3,7 @@ import {
   Toast,
   ToastClose,
   ToastDescription,
+  ToastProvider,
   ToastTitle,
   ToastViewport,
 } from "@/components/ui/toast";
@@ -10,7 +11,7 @@ import {
 export function Toaster() {
   const { toasts } = useToast();
   return (
-    <div className="fixed top-0 z-[100] flex max-h-screen w-full flex-col-reverse p-4 sm:bottom-0 sm:right-0 sm:top-auto sm:flex-col md:max-w-[420px]">
+    <ToastProvider>
       {toasts.map(function ({ id, title, description, action, ...props }) {
         return (
           <Toast key={id} {...props}>
@@ -26,6 +27,6 @@ export function Toaster() {
         );
       })}
       <ToastViewport />
-    </div>
+    </ToastProvider>
   );
 }
