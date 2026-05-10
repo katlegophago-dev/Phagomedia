e9296ee41201b446ab78c2b88bb73ac15b094076
katlegophago-dev <katlego.phago7@gmail.com>
2026-05-10 21:46:47 +0200

    Update Hero.jsx

--- src/components/home/Hero.jsx
+++ src/components/home/Hero.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from 'react';
 import { ChevronDown } from 'lucide-react';
 
 const heroImages = [
+  'https://i.imgur.com/I1JkcY2.jpeg',
   'https://i.imgur.com/sY18rVf.jpeg',
   'https://i.imgur.com/iANb9E4.jpeg',
   'https://i.imgur.com/PNMhXq5.jpeg',
-  'https://i.imgur.com/I1JkcY2.jpeg',
   'https://i.imgur.com/Lzd5uce.jpeg',
   'https://i.imgur.com/FNRGKjS.jpeg',
 ];
